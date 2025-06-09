46f5cbfb5aa0f2e06e20b6c80f153c13f616eac2
Buinoza-Olena <116521721+Buinoza-Olena@users.noreply.github.com>
2025-06-10 01:01:05 +0300

    Update buinoza_da8.py

--- buinoza_da8.py
+++ buinoza_da8.py
@@ -150,7 +150,7 @@ elif chart_option == "Огляд департаменту/ів":
         x="satisfaction_level",
         nbins=20,
         title="Розподіл задоволеності",
-        labels={"satisfaction_level": "Рівень задоволеності", "count": "Кількість працівників"}
+        labels={"satisfaction_level": "Рівень задоволеності", "к-ть працівників": "Кількість працівників"}
     )
     fig.update_layout(bargap=0.1)
     st.plotly_chart(fig, use_container_width=True)
